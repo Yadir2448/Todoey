82e6e9ccca09895628a0e920e51a828c775362f9
Yadir Jimenez <Yadirandmelissa@yadirs-macbook-pro.local>
2018-05-14 17:11:44 -0400

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yadir Jimenez on 5/14/18.
+//  Copyright © 2018 YadirJ. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
